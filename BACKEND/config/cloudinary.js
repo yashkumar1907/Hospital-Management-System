e0b9864287fad84e66bd1aa084336c7553425732
yashkumar1907 <yashkumar9926@gmail.com>
2026-08-03 11:02:44 +0530

    L

--- BACKEND/config/cloudinary.js
+++ BACKEND/config/cloudinary.js
@@ -6,4 +6,7 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
+console.log(process.env.CLOUDINARY_CLOUD_NAME);
+console.log(process.env.CLOUDINARY_API_KEY);
+
 module.exports = cloudinary;
